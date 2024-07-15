d4415c0a02cbc0a415fe4843490c1fb90ee6bb40
Sergey Markov <markov_ss8@mail.ru>
2024-07-15 19:32:39 +0300

    header in progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
+let burger = document.querySelector('.header__burger');
+let burgerEscape = document.querySelector('.header__escape');
+let burgerMenu = document.querySelector('.header__burger-menu'); 
 
+burger.addEventListener('click', () => {
+    burgerMenu.classList.add('header__burger-menu_visible');
+})
+
+burgerEscape.addEventListener('click', () => {
+    burgerMenu.classList.remove('header__burger-menu_visible');
+})
