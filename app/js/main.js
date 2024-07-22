4b72aa7cf7840161b06316b3f5e2bb82f4bc5b92
Sergey Markov <markov_ss8@mail.ru>
2024-07-22 22:26:17 +0300

    footer desktop

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,16 @@ let burgerEscape = document.querySelector('.header__escape');
 let burgerMenu = document.querySelector('.header__burger-menu'); 
 
 burger.addEventListener('click', () => {
-    burgerMenu.classList.add('header__burger-menu_visible');
+    if (burger.classList.contains('header__burger_active')) {
+        burgerMenu.classList.remove('header__burger-menu_visible');
+        burger.classList.remove('header__burger_active');
+    } else {        
+        burgerMenu.classList.add('header__burger-menu_visible');
+        burger.classList.add('header__burger_active');
+    }
 })
 
 burgerEscape.addEventListener('click', () => {
     burgerMenu.classList.remove('header__burger-menu_visible');
+    burger.classList.remove('header__burger_active');
 })
