cb4d3f331da8d0b10afbe444376d3afc91cfc0ce
Sergey Markov <markov_ss8@mail.ru>
2024-07-26 17:18:35 +0300

    footer - mobile

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 let burger = document.querySelector('.header__burger');
 let burgerEscape = document.querySelector('.header__escape');
 let burgerMenu = document.querySelector('.header__burger-menu'); 
+let footerServises = document.querySelector('.footer__services'); 
+let footerInfo = document.querySelector('.footer__info'); 
 
 burger.addEventListener('click', () => {
     if (burger.classList.contains('header__burger_active')) {
@@ -15,4 +17,20 @@ burger.addEventListener('click', () => {
 burgerEscape.addEventListener('click', () => {
     burgerMenu.classList.remove('header__burger-menu_visible');
     burger.classList.remove('header__burger_active');
+})
+
+footerServises.addEventListener('click', () => {
+    if (footerServises.classList.contains('footer__services_expanded')) {
+        footerServises.classList.remove('footer__services_expanded');
+    } else {        
+        footerServises.classList.add('footer__services_expanded');
+    }
+})
+
+footerInfo.addEventListener('click', () => {
+    if (footerInfo.classList.contains('footer__info_expanded')) {
+        footerInfo.classList.remove('footer__info_expanded');
+    } else {        
+        footerInfo.classList.add('footer__info_expanded');
+    }
 })
